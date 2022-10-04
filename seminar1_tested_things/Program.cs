55ec632047077dae07d793870664f856ebc52956
Alexander Shar <alahan05@gmail.com>
2022-10-04 14:59:05 +0300

    Example of new random(rng) and setting cursor

--- seminar1_tested_things/Program.cs
+++ seminar1_tested_things/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+int numberA = new Random().Next(1,10);
+int numberB = new Random().Next(1,10);
+
+System.Console.WriteLine(numberA);
+System.Console.WriteLine(numberB);
+*/
+
+System.Console.Write("write sasha with any registr on any symbols: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "sasha")
+{
+    Console.Write("it works!");
+}
+else
+{
+    System.Console.WriteLine("smth goes wrong(((");
+}
